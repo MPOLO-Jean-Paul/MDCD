642733a60ad3f00949a5426f3b6b17a9cf827e23
Jean Paul Mpolo <jeanpaulmpolo242@gmail.com>
2026-04-07 13:44:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9584386976-fc161",
+  "appId": "1:783142395952:web:9966c1f3af531014d78df5",
+  "apiKey": "AIzaSyAHrBu121Dbd6gOWq8qGw1V9IPd6S2L-zk",
+  "authDomain": "studio-9584386976-fc161.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "783142395952"
+};
